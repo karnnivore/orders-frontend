32d39a9aa99c81bd3e0fdf6cf04d39ab3322ce09
Nick Chinsen <nicholaschinsen@gmail.com>
2022-04-16 20:44:59 -0400

    Update index to tsx

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { Container } from '@mui/material'
+import { Orders } from './orders'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      
+      <Container>
+        <main>
+          <div className="grid">
+            <Orders/>
+          </div>
+        </main>
+      </Container>
+    </div>
+  )
+}
