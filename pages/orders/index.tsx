18a2d488be463a9edeb49da38bacb23deb9aa975
Nick Chinsen <nicholaschinsen@gmail.com>
2022-04-17 21:29:58 -0400

    Get orders from local api

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,26 +1,48 @@
 import { Paper } from "@mui/material"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { AllOrders } from "../../components/orders/AllOrders"
 import { OrderFilters } from "../../components/orders/OrderFilters"
 import { IFilters } from "../../models/IFilters"
 import { IOrders } from "../../models/IOrders"
 import data from './orders.json'
 
-const orders = data.orders 
-
 export default function Orders() {
-  const [orderData, setOrderData] = useState<IOrders[]>(orders)
+  const [orderData, setOrderData] = useState<IOrders[]>([])
   const [filters, setFilters] = useState<IFilters>({
     status: '',
     size: '',
     condition: '',
     type: ''
   })
+  const [loaded, setLoaded] = useState(false)
+
+  // fetch data from api
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }
+    fetch('http://localhost:8081/orders', requestOptions)
+      .then(response => response.json())
+      .then((data: IOrders[]) => {
+        setOrderData(data)
+        setLoaded(true)
+        console.log('data loaded: ', data)
+      })
+  }, [])
 
   return (
     <Paper>
-      <OrderFilters data={orderData} filters={filters} setFilters={setFilters}/>
-      <AllOrders orderData={orderData} filters={filters}/>
+      {loaded ?
+        <>
+          <OrderFilters data={orderData} filters={filters} setFilters={setFilters}/>
+          <AllOrders orderData={orderData} filters={filters}/>
+        </>
+      :
+        <>
+          <h2>Loading</h2>
+        </>
+      }
     </Paper>
   )
 }
